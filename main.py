58105437230a680ef6857db9d21b5a77574a419d
Dilyn Fullerton <dilyn.fullerton@att.net>
2015-11-23 15:35:45 -0800

    Implemented 4 state busy beaver input files

--- main.py
+++ main.py
@@ -7,34 +7,13 @@ from turing import tape_from_file
 
 
 def main():
-    #
-    # bb4_transition_fn = TransitionFunction(
-    #     {0: {0: (1, 'r', 1),
-    #          2: (0, 'l', -1),
-    #          1: (1, 'l', 1)},
-    #      1: {0: (1, 'l', 0),
-    #          2: (0, 'l', -1),
-    #          1: (0, 'l', 2)},
-    #      2: {0: (1, 'r', 'HALT'),
-    #          2: (0, 'l', -1),
-    #          1: (1, 'l', 3)},
-    #      3: {0: (1, 'r', 3),
-    #          2: (0, 'l', -1),
-    #          1: (0, 'r', 0)},
-    #      -1: {0: (0, 'r', 0),
-    #           2: (2, 'r', 0),
-    #           1: (1, 'r', 0)}})
-    # bb4_tm = TuringMachine(states={0, 1, 2, 3, 'HALT'},
-    #                        alphabet={0, 1, 2},
-    #                        blank=0,
-    #                        input_alphabet={1, 2},
-    #                        initial_state=0,
-    #                        final_states={'HALT'},
-    #                        transition_function=bb4_transition_fn)
-    # bb4_result = bb4_tm.compute(input_tape=blank_tape(0, length=14,
-    #                                                   start_index=10),
-    #                             print_results=True)
-    turing_machine_from_file('examples/bb3.txt').compute(
-        input_tape=tape_from_file('examples/bb3_input.txt'), print_results=True
+    busy_beaver3 = turing_machine_from_file('examples/bb3.txt')
+    busy_beaver3.compute(
+        input_tape=tape_from_file('examples/bb3_input.txt'), print_results=False
+    )
+
+    busy_beaver4 = turing_machine_from_file('examples/bb4.txt')
+    busy_beaver4.compute(
+        input_tape=tape_from_file('examples/bb4_input.txt'), print_results=True
     )
 main()
